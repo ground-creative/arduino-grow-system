6babb730cb7946028091e035b40eff7a70f36134
ground-creative <irony00100@gmail.com>
2022-10-07 00:42:30 +0700

    Update README.md

--- RelayControllerWiFiDefaultConfig.h
+++ RelayControllerWiFiDefaultConfig.h
@@ -3,26 +3,11 @@
 
 	/********* START CONFIG *******************************/
 
-	// System ID
-	//const String roomID = "kroom";
-
-	// Network credentials
-	//const char* ssid = "";
-	//const char* password = "";
-
-	// MQTT Broker:
-	//const char *mqtt_server = "";
-	//const char *mqtt_username = "";
-	//const char *mqtt_password = "";
-
 	// Esp-01
 	#define RXD2 3
 	#define TXD2 1
 	//#define WIFI_LED_PIN 2
 	//#define MQTT_LED_PIN 0	
-	
-	//#define RXD2 3
-	//#define TXD2 1
 	
 	// Esp32 dev kit
 	//#define RXD2 16
